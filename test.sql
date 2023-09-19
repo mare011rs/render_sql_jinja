61ea97b690b236484d93e4312707a0085a023552
mare011rs <mare011rs@gmail.com>
2023-09-19 11:09:54 +0200

    update test.sql example

--- test.sql
+++ test.sql
@@ -1,4 +1,5 @@
 {% set test_variable = filter_values('FILTER_2') if filter_values('FILTER_2') else [1, 2, 3] %}
+{% set test_variable2 = filter_values('FILTER_3')[0] if filter_values('FILTER_3') else 'Test' %}
 
 select
 	*
@@ -12,3 +13,5 @@ where 1=1
   {% endif %}
   
   and metric_2 in {{ test_variable|where_in }}
+
+  and metric_3 = '{{ test_variable2 }}'
--- test_render.sql
+++ test_render.sql
@@ -8,4 +8,4 @@ where 1=1
   
   and metric_2 in (1, 2, 3)
 
-  and metric_3  = None
+  and metric_3 = 'Test'
